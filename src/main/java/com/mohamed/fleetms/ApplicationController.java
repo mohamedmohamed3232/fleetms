8f1d01fc57fba67dfea98ecb8a063558072782b9
mohamedmohamed3232 <44126576+mohamedmohamed3232@users.noreply.github.com>
2023-02-26 14:58:56 -0500

    Added a layout HTMl page in order to have the layouts for all future pages. Also added all the departments and their index pages

--- src/main/java/com/mohamed/fleetms/ApplicationController.java
+++ src/main/java/com/mohamed/fleetms/ApplicationController.java
@@ -9,8 +9,24 @@ public class ApplicationController {
     public String home() {
         return "index";
     }
-    @GetMapping("/widgets")
-    public String widgets() {
-        return "widgets";
+    @GetMapping("/hr")
+    public String hr() {
+        return "/hr/index";
+    }    @GetMapping("/fleet")
+    public String fleet() {
+        return "/fleet/index";
+    }    @GetMapping("/helpdesk")
+    public String helpdesk() {
+        return "/helpdesk/index";
+    }    @GetMapping("/accounts")
+    public String accounts() {
+        return "/accounts/index";
+    }    @GetMapping("/payroll")
+    public String payroll() {
+        return "/payroll/index";
+    }
+    @GetMapping("/parameters")
+    public String parameters() {
+        return "/parameters/index";
     }
 }
